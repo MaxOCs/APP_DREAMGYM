dd1ec54e311df86c855f83d6b429801968a25d64
Maximiliano Ortiz <MaxOC.2001@outlook.com>
2024-05-23 15:39:31 -0700

    Login funcional

--- views/HomeScren.jsx
+++ views/HomeScren.jsx
@@ -10,7 +10,7 @@ const HomeScreen = ({ navigation }) => {
   const [contraseña, setContraseña] = useState('');
   const [error, setError] = useState('');
 
-  
+  //Funcion login sin backend (No borrar!!!! me sirve para saltarme el login)
   const handleLogin = () => {
     console.log('Nombre de usuario:', nombre); // Verifica el valor de nombre
     if (nombre === '') {
@@ -19,6 +19,40 @@ const HomeScreen = ({ navigation }) => {
     }
     navigation.navigate('Principal', {screen: 'Inicio',params:{nombreUsuario:nombre}});
   };
+
+    // Método para manejar la navegación entre pantallas con parámetros
+    const handleNavigation = (screenName, params = {}) => {
+      navigation.navigate(screenName, params);
+    };
+  
+    // Función para manejar el inicio de sesión (Josue implentalo con el diseño actual)
+    const handleLogin2 = async () => {
+      const queryParams = new URLSearchParams({ nombre, password }).toString();
+      const url = `${webservice}/login?${queryParams}`;
+  
+      try {
+        const response = await fetch(url, {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        });
+  
+        console.log('Valor de nombre antes de navegar:', nombre);
+  
+        if (response.ok) {
+          handleNavigation('Principal', { screen: 'Inicio',params:{nombreUsuario:nombre}}); //enviar el nombre del Usuario 
+        } else {
+          const errorData = await response.json();
+          setError(errorData.error || 'Error en el registro');
+        }
+      } catch (error) {
+        console.error('Error de red:', error.message);
+        setError('Error de red');
+      }
+    };
+
+  
 
   return (
     <View style={styles.container}>
@@ -41,7 +75,7 @@ const HomeScreen = ({ navigation }) => {
         Registrarme
       </Text>
       <BotonPrincipal 
-        onPress={handleLogin} 
+        onPress={handleLogin} // aqui se debe llamar el incio de sesion 
         title="Iniciar sesión"  
       />
       {error && <Text style={{ color: 'red' }}>{error}</Text>}
